bbb26f3aab794d6166a3cf9c6da290bb062c7163
Zhanna Suianina <cezyby3@gmail.com>
2025-08-24 19:50:28 +0300

    Edited the project, corrected files, styles and errors

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,15 +7,15 @@ const HomePage = () => {
     return (
       <>
         <Header/>
-        <div className={css.heroImg}>
-          <Container>
+        <Container>
+          <div className={css.heroImg}>
             <div className={css.container}>
               <h1 className={css.title}>Campers of your dreams</h1>
                 <p className={css.text}>You can find everything you want in our catalog</p>
             <Link to="/catalog" className={css.btn}>View Now</Link>
             </div>
-          </Container>
-        </div>
+          </div>
+        </Container>
       </>
     );
 }
